cd7b9aeaf0f6a003e7c92dafe1da67922a7eaf1c
Jan Fischlmayr <jan.fischlmayr@gmx.net>
2020-05-25 08:49:31 +0200

    Tests noDecimal and ValidCurrencyString work

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
 
-    private static final String PATTERN = "\\$\\d+(\\.\\d{1,2})?";
+    private static final String PATTERN = "\\$\\s\\d+(\\.\\d{1,2})?";
     private static final Pattern pattern = Pattern.compile(PATTERN);
 
     public static Matcher getMatcher(String usCurrencyString) {
